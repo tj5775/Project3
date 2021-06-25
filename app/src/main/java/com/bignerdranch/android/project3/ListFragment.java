e9920b1fcde15f112f4ea455d7cd954145bd1fb4
tj5775 <tj.5775@gmail.com>
2021-06-25 00:10:02 -0700

    All 4 Files commented Done

--- app/src/main/java/com/bignerdranch/android/project3/ListFragment.java
+++ app/src/main/java/com/bignerdranch/android/project3/ListFragment.java
@@ -41,7 +41,8 @@ public class ListFragment extends Fragment {
         dataList = new ArrayList<>();
 
         if (getArguments() != null) {
-            dataList = getArguments().getStringArrayList("data");
+            // receiving the data from LeftFragment
+            dataList = getArguments().getStringArrayList("data"); // dataList contains make and model IDs
         }
     }
 
@@ -53,35 +54,36 @@ public class ListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list_layout, container, false);
         mListView = view.findViewById(R.id.listView);
-        mListView.setAdapter(null);
+        mListView.setAdapter(null);                     // clears the list view
         mProgressBar = view.findViewById(R.id.progressbar);
-        mProgressBar.setVisibility(View.INVISIBLE);
-        if(!dataList.isEmpty()) {
+        mProgressBar.setVisibility(View.INVISIBLE);     // set progress bar to invisible
+        if(!dataList.isEmpty()) {                       // if the received data is not empty
             available_models = new ArrayList<>();
             makeModelIDs = new ArrayList<>();
 
-            setListView(view);
-            setListViewListener();
+            setListView(view);                          // calls setListView method
+            setListViewListener();                      // calls setListViewListener method
         }
 
 
 
         return view;
     }
+
     //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    // setListView method sets the list view with the available cars list and thier info
 
     private void setListView(View view){
         available_models.clear();
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(new Runnable() {
             @Override
-            public void run() {
-                //doInBackground
+            public void run() {                             //doInBackground
                 Log.d("doInBackground", "HERE");
-                mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.VISIBLE);   // set progress bar visible while loading available car's data
 
                 HttpHandler sh = new HttpHandler();
-
+                // getting the string from the URL
                 String jsonStr= sh.makeServiceCall(getModelAvailableURL(dataList.get(0), dataList.get(1)));
 
                 if (jsonStr != null) {
@@ -92,20 +94,20 @@ public class ListFragment extends Fragment {
                         for (int i = 0; i < contacts.length(); i++) {
                             JSONObject d = contacts.getJSONObject(i);
 
-                            String model = d.getString("model");
-                            String make = d.getString("vehicle_make");
-                            String itemNumb = "Vehicle # " + (i + 1);
-                            String price = d.getString("price");
-                            String id = d.getString("id");
-                            String mileage = d.getString("mileage");
-                            makeModelIDs.add(id);
-                            HashMap<String, String> contact = new HashMap<>();
-                            contact.put("itemNumb" , itemNumb);
-                            contact.put("model", model);
-                            contact.put("make", make);
-                            contact.put("price", price);
-                            contact.put("mileage", mileage);
-                            available_models.add(contact);
+                            String model = d.getString("model");        // getting models
+                            String make = d.getString("vehicle_make");  // getting makes
+                            String itemNumb = "Vehicle # " + (i + 1);         // Vehicle numbers in list
+                            String price = d.getString("price");        // getting vehicles' prices
+                            String id = d.getString("id");              // getting vehicles' IDs
+                            String mileage = d.getString("mileage");    // getting mileages
+                            makeModelIDs.add(id);                             // storing IDs in array list
+                            HashMap<String, String> car = new HashMap<>();// hashmap to store vehicles info
+                            car.put("itemNumb" , itemNumb);
+                            car.put("model", model);
+                            car.put("make", make);
+                            car.put("price", price);
+                            car.put("mileage", mileage);
+                            available_models.add(car);                     // stores cars in available_models array list
                         }
 
                     } catch (JSONException e) {
@@ -120,7 +122,7 @@ public class ListFragment extends Fragment {
                         // postExecute
                         Log.d("postExecute", "HERE");
 
-                        if(available_models.isEmpty()){
+                        if(available_models.isEmpty()){                     // if there are no available cars
                             HashMap<String, String> contact = new HashMap<>();
                             contact.put("itemNumb", "Vehicles NOT available");
                             contact.put("model", "Try a different make or model");
@@ -129,12 +131,12 @@ public class ListFragment extends Fragment {
                             available_models.add(contact);
                         }
 
-
+                        // sets up adapter
                         ListAdapter adapter = new SimpleAdapter(getContext(), available_models,
                                 R.layout.model_list, new String[] {"itemNumb", "model", "price", "mileage",},
                                 new int[]{R.id.txtViewItemNumber, R.id.txtViewModels, R.id.txtViewPrices, R.id.txtViewMileages});
                         mListView.setAdapter(adapter);
-                        mProgressBar.setVisibility(View.INVISIBLE);
+                        mProgressBar.setVisibility(View.INVISIBLE);     // sets progress bar invisible
                     }
                 });
             }
@@ -142,7 +144,10 @@ public class ListFragment extends Fragment {
 
     }
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-private void setListViewListener(){
+    // setListViewListener method  set a click listener on the list view, which send the user to
+    // RightFragment makeModel IDs, make and model selected
+
+    private void setListViewListener(){
     mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -150,18 +155,19 @@ private void setListViewListener(){
             String model = available_models.get(index).get("model");
             String make = available_models.get(index).get("make");
 
-            if(!model.equals("Try a different make or model")) {
+            if(!model.equals("Try a different make or model")) {// if there are model available
                 ArrayList<String> temp = new ArrayList<>();
                 temp.add(makeModelIDs.get(index));
                 temp.add(make);
                 temp.add(model);
 
-                mCommunicatorList.passDataListToRight(temp);
+                mCommunicatorList.passDataListToRight(temp);    // send data to RightFragment
             }
         }
     });
 }
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    // getModelAvailableURL method returns a string with the URL + car ID + model ID + a defined zip code
 
     public String getModelAvailableURL(String make, String model){
         String modelsAvailable = url + "cars/" + make + "/" + model + "/92603";
@@ -184,6 +190,6 @@ private void setListViewListener(){
     @Override
     public void onDetach() {
         super.onDetach();
-        mCommunicatorList = null;
+        mCommunicatorList = null;       // sets mCommunicatorList to null
     }
 }
